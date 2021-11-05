c79e27a84f5a8bf832b1049dfa0040366dac4645
Hashir-Khan777 <khnhashir555@gmail.com>
2021-11-05 15:37:01 +0500

    some changes

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -324,18 +324,58 @@ function Home() {
             </div>
             <div className="col-lg-4 service_col">
               <img loading="lazy" src="images/exterior.jpg" alt="" />
+              <div className="service_image_overlay">
+                <a href="#book">
+                  <button className="btn btn-primary number">book now</button>
+                </a>
+                <a href="javascript:void(Tawk_API.toggle())">
+                  <button className="btn btn-primary number">chat now</button>
+                </a>
+              </div>
             </div>
             <div className="col-lg-4 service_col">
               <img loading="lazy" src="images/interior.jpg" alt="" />
+              <div className="service_image_overlay">
+                <a href="#book">
+                  <button className="btn btn-primary number">book now</button>
+                </a>
+                <a href="javascript:void(Tawk_API.toggle())">
+                  <button className="btn btn-primary number">chat now</button>
+                </a>
+              </div>
             </div>
             <div className="col-lg-4 service_col">
               <img loading="lazy" src="images/bedroom.jpg" alt="" />
+              <div className="service_image_overlay">
+                <a href="#book">
+                  <button className="btn btn-primary number">book now</button>
+                </a>
+                <a href="javascript:void(Tawk_API.toggle())">
+                  <button className="btn btn-primary number">chat now</button>
+                </a>
+              </div>
             </div>
             <div className="col-lg-4 service_col">
               <img loading="lazy" src="images/kitchen.jpg" alt="" />
+              <div className="service_image_overlay">
+                <a href="#book">
+                  <button className="btn btn-primary number">book now</button>
+                </a>
+                <a href="javascript:void(Tawk_API.toggle())">
+                  <button className="btn btn-primary number">chat now</button>
+                </a>
+              </div>
             </div>
             <div className="col-lg-4 service_col">
               <img loading="lazy" src="images/bathroom.jpg" alt="" />
+              <div className="service_image_overlay">
+                <a href="#book">
+                  <button className="btn btn-primary number">book now</button>
+                </a>
+                <a href="javascript:void(Tawk_API.toggle())">
+                  <button className="btn btn-primary number">chat now</button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -345,10 +385,13 @@ function Home() {
       <section className="testimonial_section">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-lg-6 col-12 testimonial_image testimonial_col"></div>
+            <div className="testimonial_image col-lg-6">
+              <img src="/images/image4.jpeg" alt="" />
+              <div className="image_overlay"></div>
+            </div>
             <div
               id="book"
-              className="col-lg-6 col-12 testimonial_col"
+              className="col-lg-6"
               style={{
                 backgroundColor: "#091e2a",
                 color: "#fff",
@@ -358,7 +401,6 @@ function Home() {
               <h1>Book a Visit Today</h1>
               <div className="row">
                 <div className="col book_form_col">
-                  <label htmlFor="testimonial_name">Name</label>
                   <input
                     type="text"
                     placeholder="Your Name"
@@ -370,7 +412,6 @@ function Home() {
                   />
                 </div>
                 <div className="col book_form_col">
-                  <label htmlFor="testimonial_phone">Phone Number</label>
                   <input
                     type="text"
                     id="testimonial_phone"
@@ -384,7 +425,6 @@ function Home() {
               </div>
               <div className="row">
                 <div className="col book_form_col">
-                  <label htmlFor="testimonial_budget">Number of persons</label>
                   <select
                     name
                     id="testimonial_budget"
@@ -394,7 +434,7 @@ function Home() {
                       setBookVisit({ ...bookVisit, persons: e.target.value })
                     }
                   >
-                    <option value={0}>Select</option>
+                    <option value={0}>Number of persons</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -403,7 +443,6 @@ function Home() {
                   </select>
                 </div>
                 <div className="col book_form_col">
-                  <label htmlFor="testimonial_time">Time</label>
                   <select
                     name
                     id="testimonial_time"
@@ -438,7 +477,6 @@ function Home() {
               </div>
               <div className="row">
                 <div className="col book_form_col">
-                  <label htmlFor="testimonial_date">Date</label>
                   <input
                     type="date"
                     id="testimonial_date"
@@ -535,6 +573,7 @@ function Home() {
                     fontSize: "17px",
                     outline: "none",
                     width: "100%",
+                    borderRadius: 4,
                   }}
                   value={form.detail}
                   onChange={(e) => setForm({ ...form, detail: e.target.value })}

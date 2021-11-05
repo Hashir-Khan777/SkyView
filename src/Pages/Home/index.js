94431fbedbc35d687c17f0aaa96ec149d13bab9d
Hashir-Khan777 <khnhashir555@gmail.com>
2021-11-05 16:00:42 +0500

    one change

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -395,104 +395,106 @@ function Home() {
               style={{
                 backgroundColor: "#091e2a",
                 color: "#fff",
-                padding: "150px 70px",
+                padding: "150px 110px",
               }}
             >
-              <h1>Book a Visit Today</h1>
-              <div className="row">
-                <div className="col book_form_col">
-                  <input
-                    type="text"
-                    placeholder="Your Name"
-                    id="testimonial_name"
-                    value={bookVisit.name}
-                    onChange={(e) =>
-                      setBookVisit({ ...bookVisit, name: e.target.value })
-                    }
-                  />
+              <div className="container">
+                <h1>Book a Visit Today</h1>
+                <div className="row">
+                  <div className="col book_form_col">
+                    <input
+                      type="text"
+                      placeholder="Your Name"
+                      id="testimonial_name"
+                      value={bookVisit.name}
+                      onChange={(e) =>
+                        setBookVisit({ ...bookVisit, name: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="col book_form_col">
+                    <input
+                      type="text"
+                      id="testimonial_phone"
+                      placeholder="Your Phone"
+                      value={bookVisit.number}
+                      onChange={(e) =>
+                        setBookVisit({ ...bookVisit, number: e.target.value })
+                      }
+                    />
+                  </div>
                 </div>
-                <div className="col book_form_col">
-                  <input
-                    type="text"
-                    id="testimonial_phone"
-                    placeholder="Your Phone"
-                    value={bookVisit.number}
-                    onChange={(e) =>
-                      setBookVisit({ ...bookVisit, number: e.target.value })
-                    }
-                  />
+                <div className="row">
+                  <div className="col book_form_col">
+                    <select
+                      name
+                      id="testimonial_budget"
+                      className="testimonial_budget_select"
+                      value={bookVisit.persons}
+                      onChange={(e) =>
+                        setBookVisit({ ...bookVisit, persons: e.target.value })
+                      }
+                    >
+                      <option value={0}>Number of persons</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5+">5+</option>
+                    </select>
+                  </div>
+                  <div className="col book_form_col">
+                    <select
+                      name
+                      id="testimonial_time"
+                      className="testimonial_budget_select"
+                      value={bookVisit.time}
+                      onChange={(e) =>
+                        setBookVisit({ ...bookVisit, time: e.target.value })
+                      }
+                    >
+                      <option value={0}>Select time</option>
+                      <option value="10:00">10:00</option>
+                      <option value="10:30">10:30</option>
+                      <option value="11:00">11:00</option>
+                      <option value="11:30">11:30</option>
+                      <option value="12:00">12:00</option>
+                      <option value="12:30">12:30</option>
+                      <option value="1:00">1:00</option>
+                      <option value="1:30">1:30</option>
+                      <option value="2:00">2:00</option>
+                      <option value="2:30">2:30</option>
+                      <option value="3:00">3:00</option>
+                      <option value="3:30">3:30</option>
+                      <option value="4:00">4:00</option>
+                      <option value="4:30">4:30</option>
+                      <option value="5:00">5:00</option>
+                      <option value="5:30">5:30</option>
+                      <option value="6:00">6:00</option>
+                      <option value="6:30">6:30</option>
+                      <option value="7:00">7:00</option>
+                    </select>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col book_form_col">
-                  <select
-                    name
-                    id="testimonial_budget"
-                    className="testimonial_budget_select"
-                    value={bookVisit.persons}
-                    onChange={(e) =>
-                      setBookVisit({ ...bookVisit, persons: e.target.value })
-                    }
-                  >
-                    <option value={0}>Number of persons</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5+">5+</option>
-                  </select>
-                </div>
-                <div className="col book_form_col">
-                  <select
-                    name
-                    id="testimonial_time"
-                    className="testimonial_budget_select"
-                    value={bookVisit.time}
-                    onChange={(e) =>
-                      setBookVisit({ ...bookVisit, time: e.target.value })
-                    }
-                  >
-                    <option value={0}>Select time</option>
-                    <option value="10:00">10:00</option>
-                    <option value="10:30">10:30</option>
-                    <option value="11:00">11:00</option>
-                    <option value="11:30">11:30</option>
-                    <option value="12:00">12:00</option>
-                    <option value="12:30">12:30</option>
-                    <option value="1:00">1:00</option>
-                    <option value="1:30">1:30</option>
-                    <option value="2:00">2:00</option>
-                    <option value="2:30">2:30</option>
-                    <option value="3:00">3:00</option>
-                    <option value="3:30">3:30</option>
-                    <option value="4:00">4:00</option>
-                    <option value="4:30">4:30</option>
-                    <option value="5:00">5:00</option>
-                    <option value="5:30">5:30</option>
-                    <option value="6:00">6:00</option>
-                    <option value="6:30">6:30</option>
-                    <option value="7:00">7:00</option>
-                  </select>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col book_form_col">
-                  <input
-                    type="date"
-                    id="testimonial_date"
-                    placeholder="Your Phone"
-                    value={bookVisit.date}
-                    onChange={(e) =>
-                      setBookVisit({ ...bookVisit, date: e.target.value })
-                    }
-                  />
-                  <button
-                    type="button"
-                    className="btn btn-primary number about_button mt-4"
-                    onClick={bookavisit}
-                  >
-                    Submit Request
-                  </button>
+                <div className="row">
+                  <div className="col book_form_col">
+                    <input
+                      type="date"
+                      id="testimonial_date"
+                      placeholder="Your Phone"
+                      value={bookVisit.date}
+                      onChange={(e) =>
+                        setBookVisit({ ...bookVisit, date: e.target.value })
+                      }
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-primary number about_button mt-4"
+                      onClick={bookavisit}
+                    >
+                      Submit Request
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
